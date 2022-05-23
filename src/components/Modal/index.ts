59582d5a9653f588fb7f741d56380d56e8d1aaa1
Sotaneum <gnyontu39@gmail.com>
2022-05-24 08:17:08 +0900

    [add_modal_toast_select] Modal 컴포넌트 추가

---
+++ src/components/Modal/index.ts
@@ -0,0 +1,15 @@
+import PureController, { PureControllerOptions } from "@/core/PureController";
+import ModalComponent, { ModalComponentProps } from "./Modal";
+
+export default class ModalController extends PureController {
+  constructor(targetEl: Element, options: PureControllerOptions) {
+    super(targetEl, options);
+  }
+
+  add(props: ModalComponentProps) {
+    return new ModalComponent({
+      ...props,
+      removeDelay: this.getOption("removeDelay"),
+    }).mount(this.target);
+  }
+}
