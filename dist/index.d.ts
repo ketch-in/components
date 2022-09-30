61c7dbc01dcb72a67f1f008ebb076d51d3960c3f
joon-k <joon.k@kakaocorp.com>
2022-09-30 15:42:50 +0900

    remove css import

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
-import "./assets/styles/index.css";
 export { default as ModalController } from "./components/Modal";
 export { default as SelectModalController } from "./components/SelectModal";
 export { default as ToastController } from "./components/Toast";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "./assets/styles/index.css";
+// import "./assets/styles/index.css";
 
 export { default as ModalController } from "./components/Modal";
 export { default as SelectModalController } from "./components/SelectModal";
