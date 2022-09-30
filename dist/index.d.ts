561598b214972d039b50885c7369b7113f6c2155
joon-k <joon.k@kakaocorp.com>
2022-09-30 15:51:06 +0900

    fix module export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
-export { default as ModalController } from "./components/Modal";
-export { default as SelectModalController } from "./components/SelectModal";
-export { default as ToastController } from "./components/Toast";
-export { default as ToolbarController } from "./components/Toolbar";
+import ModalController from "./components/Modal";
+import SelectModalController from "./components/SelectModal";
+import ToastController from "./components/Toast";
+import ToolbarController from "./components/Toolbar";
+export { ModalController, SelectModalController, ToastController, ToolbarController };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 // import "./assets/styles/index.css";
 
-export { default as ModalController } from "./components/Modal";
-export { default as SelectModalController } from "./components/SelectModal";
-export { default as ToastController } from "./components/Toast";
-export { default as ToolbarController } from "./components/Toolbar";
+// export { default as ModalController } from "./components/Modal";
+// export { default as SelectModalController } from "./components/SelectModal";
+// export { default as ToastController } from "./components/Toast";
+// export { default as ToolbarController } from "./components/Toolbar";
+
+import ModalController from "./components/Modal";
+import SelectModalController from "./components/SelectModal";
+import ToastController from "./components/Toast";
+import ToolbarController from "./components/Toolbar";
+
+export { ModalController, SelectModalController, ToastController, ToolbarController };
